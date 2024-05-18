a0adca703242aa066ad32d55861f3cb091b32006
abdlmnf17 <11210195@bsi.ac.id>
2024-05-18 16:49:59 +0700

    Memperbarui Layout Laporan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -141,17 +141,17 @@
 
             <!-- Nav Item - Charts -->
             <li class="nav-item">
-                <a class="nav-link" href="charts.html">
+                <a class="nav-link" href="/laporan">
                     <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Laporan</span></a>
+                    <span>Laporan Gaji</span></a>
             </li>
 
             <!-- Nav Item - Tables -->
-            {{-- <li class="nav-item">
-                <a class="nav-link" href="tables.html">
+            <li class="nav-item">
+                <a class="nav-link" href="/laporanjurnal">
                     <i class="fas fa-fw fa-table"></i>
-                    <span>Contoh Menu</span></a>
-            </li> --}}
+                    <span>Laporan Jurnal Umum</span></a>
+            </li>
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
